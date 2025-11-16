a7ea17009d57e497f34ac682c815ee9a26dd0811
ENux-Distro <emir73503@gmail.com>
2025-11-16 02:54:24 -0500

    Update phase2.sh

--- phase2.sh
+++ phase2.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
+
 # Must run as root
 if [ "$EUID" -ne 0 ]; then
-  echo: "======================================="
-  echo: "= ERROR: phase2.sh must run as root!" ="
-  echo: "======================================="
+  echo "======================================="
+  echo "=   ERROR: phase2.sh must run as root  ="
+  echo "======================================="
   exit 1
 fi
 
+# Prevent double-running
 if [ -f /etc/enux-phase2-done ]; then
   exit 0
 fi
 
 clear
 echo "========================================"
-echo     "ENux PHASE 2 - INSTALLING E's "
+echo "         ENux PHASE 2 - INSTALLING E's"
 echo "========================================"
+echo
 
 echo "Fetching Arch..."
 brl fetch arch
 
-echo "Fetching Fedora..."
+echo "Fetching Fedora ..."
 brl fetch fedora --release 41
 
 echo "Fetching Void..."
 brl fetch void
 
-echo "Fetching Alpine"
+echo "Fetching Alpine..."
 brl fetch alpine
 
-echo "Fetching Gentoo"
+echo "Fetching Gentoo..."
 brl fetch gentoo
 
 echo
 echo "========================================"
 echo "=          ENux 1.0 IS READY!          ="
-echo "=           Run: enuxfetch             ="
+echo "=         Run: enuxfetch anytime       ="
 echo "========================================"
+echo
 
+# Mark phase 2 as done
+mkdir -p /etc
 touch /etc/enux-phase2-done
